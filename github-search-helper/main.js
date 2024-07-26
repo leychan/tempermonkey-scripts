7d7263ae596a7936dd15350fe2f405a35ce960f4
lei <sanyue0315@outlook.com>
2024-07-26 10:25:52 +0800

    优化

--- github-search-helper/main.js
+++ github-search-helper/main.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         github-search-helper
 // @namespace    github.com/leychan
-// @version      0.1
-// @description  在新标签页打开github搜索结果
+// @version      0.2
+// @description  在新标签页打开github搜索,explore和trending结果
 // @author       leychan
 // @match        https://github.com/trending
 // @match        https://github.com/explore
@@ -15,21 +15,28 @@
 
 (function() {
     'use strict';
-  
-    const element = document.querySelector('[data-testid="results-list"]');
-    if (element) {
-        let allSearchResultATag = element.getElementsByTagName('a')
-        for (let i = 0 ;i < allSearchResultATag.length; i++) {
-            allSearchResultATag[i].setAttribute('target', '_blank')
+
+    const observer = new MutationObserver((mutationsList, observer) => {
+        const element = document.querySelector('[data-testid="results-list"]');
+        if (element) {
+            let allSearchResultATag = element.getElementsByTagName('a')
+            for (let i = 0 ;i < allSearchResultATag.length; i++) {
+                allSearchResultATag[i].setAttribute('target', '_blank')
+                console.log(allSearchResultATag[i])
+            }
+            observer.disconnect(); // 停止观察
         }
-    }
-    
-    let allExploreResultATag = exploreResult.getElementsByTagName('a')
+    });
+
+    observer.observe(document.body, { childList: true, subtree: true });
+
+
+    let allExploreResultATag = document.getElementsByTagName('a')
     for (let i = 0 ;i < allExploreResultATag.length; i++) {
         allExploreResultATag[i].setAttribute('target', '_blank')
     }
 
-    let allTrendingResultATag = trendingResult.getElementsByTagName('a')
+    let allTrendingResultATag = document.getElementsByTagName('a')
     for (let i = 0 ;i < allTrendingResultATag.length; i++) {
         allTrendingResultATag[i].setAttribute('target', '_blank')
     }
